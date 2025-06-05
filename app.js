1322fa916433704734581c9194b725b45a284001
Carlos Bermúdez <cbermudezg7@gmail.com>
2025-06-04 23:23:46 -0500

    last cors

--- app.js
+++ app.js
@@ -9,32 +9,6 @@ const cors = require('cors');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
-// Definir la configuración para Swagger (Documentación)
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API de Prestamos',
-      version: '1.0.0',
-      description: 'Documentación de la API de Prestamos',
-    },
-  },
-  apis: ['./routes/*.js'], // Ruta a las rutas donde están los comentarios de Swagger
-};
-const swaggerDocs = swaggerJsdoc(swaggerOptions);
-
-// Configurar la ruta para acceder a la documentación de Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-//Fin de la documentación
-
-// Aumenta el límite de tamaño del cuerpo de la solicitud
-app.use(express.json({ limit: "10mb" })); // Aumenta el límite a 10MB
-app.use(express.urlencoded({ limit: "10mb", extended: true })); 
-
-app.use(helmet({
-    crossOriginResourcePolicy: false,
-}));
-
 // Lista de orígenes permitidos
 const allowedOrigins = ['https://autentic.ec', 'https://app.autentic.ec', 'http://localhost:5173'];
 
@@ -49,6 +23,32 @@ app.use(cors({
     }
   },
   credentials: false // si usas cookies o auth headers
+}));
+
+// Definir la configuración para Swagger (Documentación)
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Prestamos',
+      version: '1.0.0',
+      description: 'Documentación de la API de Prestamos',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+// Configurar la ruta para acceder a la documentación de Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+//Fin de la documentación
+
+// Aumenta el límite de tamaño del cuerpo de la solicitud
+app.use(express.json({ limit: "10mb" })); // Aumenta el límite a 10MB
+app.use(express.urlencoded({ limit: "10mb", extended: true })); 
+
+app.use(helmet({
+    crossOriginResourcePolicy: false,
 }));
 
 // Middleware para procesar JSON
